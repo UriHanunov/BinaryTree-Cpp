62c1c19f017393564f4b07b362d2c1cb2ce9fe97
Uri Hanunov <44783443+UriHanunov@users.noreply.github.com>
2019-03-31 19:11:01 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,19 +1,41 @@
 #include <iostream>
+using namespace std;
 
 namespace ariel
 {
 	class Tree
 	{
-	public:
-		void insert(int i);
-		void remove(int i);
-		int size();
-		bool contains(int i);
-		int root();
-		int parent(int i);
-		int left(int i);
-		int right(int i);
-		void print();
+		struct node
+		{
+			int value;
+			node *left;
+			node *right;
+			node *parent;
+		};
+
+		public:
+			Tree();
+			~Tree();
+
+			void insert(int key);
+			void remove(int key);
+			int size();
+			bool contains(int key);
+			int root();
+			int parent(int key);
+			int left(int key);
+			int right(int key);
+			void print();
+			node *search(int key);
+			void destroy_tree();
+
+		private:
+			node *root2;
+			void destroy_tree(node *leaf);
+			node *search(int key, node *leaf);
+			void insert(int key, node *leaf);
+			void inorder_print(node *leaf);
+
 	};
 	
 };
